3a4f3fbecf32c9ce895f6fec99f16b99b93de859
Alejandro Rodriguez <cf@MacBook-Air.Home>
2024-07-07 23:45:04 +0100

    version 4.0

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ def macbook():
     print("Esta seccao de codigo e feita no macbook")
 
 
+def juntar():
+    macbook()
+    asus_tuf()
+
+
 def main():
     print("Este codigo es para aprender github")
+    juntar()
 
 
 if __name__ == "__main__":
